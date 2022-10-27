e5e930388d7ae0e4b2b32a03b4246543dd63fba2
cyjeong2 <jcyjjang95@gmail.com>
2022-10-27 17:55:48 +0900

    diary 앱에 memory_detail 뷰 구현하고, /diary/pk/ 주소에 매핑

--- diary/urls.py
+++ diary/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>/', views.memory_detail),
     path('', views.index)
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from .models import Memory
 
 def index(request) :
+    posts = Memory.objects.all().order_by('-pk')
     return render(
         request,
         'diary/index.html',
+        {
+            'posts' : posts,
+        }
     )
+
+def memory_detail  (request, pk):
+    post = Memory.objects.get(pk=pk)
+    
+    return render(
+        request,
+        'diary/memory_detail.html',
+        {
+            'post':post,
+        }
+    )   
+
